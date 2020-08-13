00db823e879f5c9a1af221f9135e3a6fb714c384
shruti <shrutibansal1802@gmail.com>
2020-08-13 19:50:25 +0530

    particles animation

--- script.js
+++ script.js
@@ -2,38 +2,86 @@ let canvas = document.getElementById("draw");
 let ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.strokeStyle = "#ff0000"
 
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round'
-ctx.lineWidth = 50;
+let hue =0;
 
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
-let hue =0
-
-
-
-function draw(e) {
-
-  if (!isDrawing)   return;
-  
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
-  ctx.beginPath()
-  ctx.moveTo(lastX, lastY);
-  ctx.lineTo(e.offsetX, e.offsetY)
-  ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-  hue++;
-  if(hue>360)
-  hue=0;
-
+let mouse= {
+    x:undefined,
+    y: undefined
 }
 
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mousedown", (e) => {
-  isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY]
+window.addEventListener('mousemove' ,e=>{
+    mouse.x= e.x;
+    mouse.y = e.y;
+    
 })
-canvas.addEventListener("mouseup", () => isDrawing = false)
+function Circle(x,y,radius,dx,dy,hue){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.minRadius = radius;
+    this.dx = dx;
+    this.dy = dy;
+
+    this.draw = function(){
+        ctx.beginPath();
+        ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+        ctx.fillStyle = `hsl(${hue}, 100%, 50%)`;
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI *2,true);
+       ctx.fill();
+        
+    }
+
+    this.update = function(){
+    if(this.x+ this.radius >innerWidth || this.x - this.radius<0)
+            {this.dx =-this.dx;}
+   
+    if(this.y+ this.radius>innerHeight || this.y - this.radius<0)
+         {this.dy =-this.dy;}
+
+     this.x+=this.dx;
+    this.y+= this.dy;
+
+    if(mouse.x - this.x < 50 && mouse.x -this.x >-50 && 
+        mouse.y - this.y < 50 && mouse.y -this.y >-50 && this.radius <50)
+      { this.radius+=1;}
+    else if(this.radius>this.minRadius)
+    {
+        this.radius--;
+    }
+
+
+    this.draw();
+    }
+}
+
+const circleArray =[];
+
+
+    for(let i=0; i<=150;i++){
+        let radius = Math.random() *4 +1;
+
+        let x = Math.random() * (innerWidth -radius*2) +radius;
+        let y = Math.random() * (innerHeight -radius*2) +radius;
+       
+        let dx =(Math.random() -0.5)*2;
+        let dy =(Math.random() -0.5)*2;
+
+        circleArray.push(new Circle(x,y,radius,dx,dy,hue));
+        hue+=10;
+    }
+
+function animate()
+{
+    requestAnimationFrame(animate)
+    ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
+
+    for(let i=0; i<circleArray.length;i++){
+        circleArray[i].update();
+    }
+
+ 
+}
+animate();
 
